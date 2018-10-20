03e0ee308bc40d14145fb1000d85f5cd10f675d4
vijay040 <dev.vijayprakash@gamil.com>
2018-10-20 17:03:16 +0530

    updated by anuradha

--- app/src/main/java/com/mmcs/societymaintainance/activity/VisitorDetailActivity.java
+++ app/src/main/java/com/mmcs/societymaintainance/activity/VisitorDetailActivity.java
@@ -1,5 +1,8 @@
 package com.mmcs.societymaintainance.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Spannable;
@@ -7,8 +10,10 @@ import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.bumptech.glide.Glide;
 import com.mmcs.societymaintainance.R;
@@ -16,31 +21,45 @@ import com.mmcs.societymaintainance.model.UnitRestMeta;
 import com.mmcs.societymaintainance.model.VisitorModel;
 import com.mmcs.societymaintainance.util.Singleton;
 
+import java.util.Calendar;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class VisitorDetailActivity extends AppCompatActivity {
     VisitorModel visitorModel;
-    TextView txtName,txt_mobile,txt_address,txtFloor,txtUnit,txtIntime,txtOuttime;
+    TextView txtName, txt_mobile, txt_address, txtFloor, txtUnit, txtIntime, txtOuttime;
     ImageView image_visitor;
     Button btn_close;
+    EditText edt_time_out;
+    static final int TIME_DIALOG_ID = 1;
+    int cur = 0;
+    int H, M;
+    Calendar calendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visitor_detail);
         visitorModel = (VisitorModel) getIntent().getSerializableExtra(getString(R.string.visitor_model));
-        txtName=findViewById(R.id.txtName);
-        txt_mobile=findViewById(R.id.txt_mobile);
-        txt_address=findViewById(R.id.txt_address);
-        txtIntime=findViewById(R.id.txtIntime);
-        txtFloor=findViewById(R.id.txtFloor);
-        txtUnit=findViewById(R.id.txtUnit);
-        txtOuttime=findViewById(R.id.txtOuttime);
-        image_visitor=findViewById(R.id.image_visitor);
-        btn_close=findViewById(R.id.btn_close);
-        if(visitorModel.getOuttime().equalsIgnoreCase(""))
+        txtName = findViewById(R.id.txtName);
+        txt_mobile = findViewById(R.id.txt_mobile);
+        txt_address = findViewById(R.id.txt_address);
+        txtIntime = findViewById(R.id.txtIntime);
+        txtFloor = findViewById(R.id.txtFloor);
+        txtUnit = findViewById(R.id.txtUnit);
+        txtOuttime = findViewById(R.id.txtOuttime);
+        image_visitor = findViewById(R.id.image_visitor);
+        btn_close = findViewById(R.id.btn_close);
+        edt_time_out = findViewById(R.id.edt_time_out);
+        calendar = Calendar.getInstance();
+        H = calendar.get(Calendar.HOUR_OF_DAY);
+        M = calendar.get(Calendar.MINUTE);
+        if (visitorModel.getOuttime().equalsIgnoreCase(""))
             btn_close.setText("Update");
+        else
+            txtOuttime.setVisibility(View.GONE);
         txtName.setText(getString(R.string.name) + visitorModel.getName());
         txt_mobile.setText(getString(R.string.mobile_no) + visitorModel.getMobile());
         txt_address.setText(getString(R.string.address) + visitorModel.getAddress());
@@ -48,6 +67,13 @@ public class VisitorDetailActivity extends AppCompatActivity {
         txtUnit.setText(getString(R.string.unit_no) + visitorModel.getUnit_no());
         txtIntime.setText(getString(R.string.time_in) + visitorModel.getIntime());
         txtOuttime.setText(getString(R.string.time_out) + visitorModel.getOuttime());
+        edt_time_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(TIME_DIALOG_ID);
+
+            }
+        });
 
         Glide.with(this).load(visitorModel.getImage()).placeholder(R.drawable.no_image).into(image_visitor);
         SpannableStringBuilder sb = new SpannableStringBuilder(txtName.getText());
@@ -95,20 +121,17 @@ public class VisitorDetailActivity extends AppCompatActivity {
         btn_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(visitorModel.getOuttime().equalsIgnoreCase(""))
-                {
-
-                }
-                else
+                if (visitorModel.getOuttime().equalsIgnoreCase("")) {
+                    updateVisitor(edt_time_out.getText().toString());
+                } else
                     finish();
             }
         });
 
     }
 
-    private void updateVisitor(String outTime)
-    {
-        Singleton.getInstance().getApi().updateVisitor("",outTime).enqueue(new Callback<UnitRestMeta>() {
+    private void updateVisitor(String outTime) {
+        Singleton.getInstance().getApi().updateVisitor("", outTime).enqueue(new Callback<UnitRestMeta>() {
             @Override
             public void onResponse(Call<UnitRestMeta> call, Response<UnitRestMeta> response) {
 
@@ -121,4 +144,39 @@ public class VisitorDetailActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+
+            case TIME_DIALOG_ID:
+                System.out.println("onCreateDialog  : " + id);
+                cur = TIME_DIALOG_ID;
+                return new TimePickerDialog(this, onTimeSetListener, H, M, false);
+
+        }
+
+        return null;
+    }
+
+    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int h, int m) {
+            // timePicker.is24HourView();
+            if (cur == TIME_DIALOG_ID) {
+                // set selected date into textview
+                if (h < 12 && h >= 0) {
+                    edt_time_out.setText(String.valueOf(h) + ":" + String.valueOf(m) + " " + "AM");
+                } else {
+                    h -= 12;
+                    if (h == 0) {
+                        h = 12;
+                    }
+                    edt_time_out.setText(String.valueOf(h) + ":" + String.valueOf(m) + " " + "PM");
+                }
+
+
+            }
+        }
+    };
 }
