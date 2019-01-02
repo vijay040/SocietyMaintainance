7567682b3060ec026aa2a5db263006be4f41f7d1
vijay040 <dev.vijayprakash@gmail.com>
2019-01-02 15:43:37 +0530

    Imroved notification

--- app/src/main/java/com/mmcs/societymaintainance/activity/SplashActivity.java
+++ app/src/main/java/com/mmcs/societymaintainance/activity/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.mmcs.societymaintainance.activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +19,8 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.mmcs.societymaintainance.R;
 import com.mmcs.societymaintainance.model.LoginModel;
 import com.mmcs.societymaintainance.util.Shprefrences;
+
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity {
     ImageView image_wlcm, image_view;
@@ -36,6 +41,38 @@ public class SplashActivity extends AppCompatActivity {
         image_wlcm.startAnimation(animBlink);
         image_view.startAnimation(animZoom_out);
         sh=new Shprefrences(this);
+
+
+
+        Intent intent = new Intent();
+
+        String manufacturer = android.os.Build.MANUFACTURER;
+
+        switch (manufacturer) {
+
+            case "xiaomi":
+                intent.setComponent(new ComponentName("com.miui.securitycenter",
+                        "com.miui.permcenter.autostart.AutoStartManagementActivity"));
+                break;
+            case "oppo":
+                intent.setComponent(new ComponentName("com.coloros.safecenter",
+                        "com.coloros.safecenter.permission.startup.StartupAppListActivity"));
+
+                break;
+            case "vivo":
+                intent.setComponent(new ComponentName("com.vivo.permissionmanager",
+                        "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"));
+                break;
+        }
+
+        List<ResolveInfo> arrayList =  getPackageManager().queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+
+        if (arrayList.size() > 0) {
+            startActivity(intent);
+        }
+
+
         // set animation listener
         Handler h = new Handler(new Handler.Callback() {
             @Override
@@ -54,7 +91,11 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 4000);
+
     }
+
+
+
 }
 
 
--- app/src/main/java/com/mmcs/societymaintainance/fcm/SocietyMessagingService.java
+++ app/src/main/java/com/mmcs/societymaintainance/fcm/SocietyMessagingService.java
@@ -1,14 +1,21 @@
 package com.mmcs.societymaintainance.fcm;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.mmcs.societymaintainance.R;
@@ -33,6 +40,34 @@ public class SocietyMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
         // Check if message contains a notification payload.
+        Log.e("**********","******************handle intent**************************");
+       /* intent = new Intent(this, VisitorNotificationActivity.class);
+        if (remoteMessage.getNotification().getTitle().equalsIgnoreCase("Hi Guest is waiting!"))
+            intent = new Intent(this, VisitorNotificationActivity.class);
+        else if (remoteMessage.getNotification().getTitle().equalsIgnoreCase("New complain registered!"))
+            intent = new Intent(this, ComplaintNotificationActivity.class);
+        else if (remoteMessage.getNotification().getTitle().equalsIgnoreCase("ALARM!")) {
+            intent = new Intent(this, EmergancyAlarmActivity.class);
+            //Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            EmergancyAlarmActivity. r = MediaPlayer.create(this, R.raw.emergency_alarm);
+            EmergancyAlarmActivity. r.setLooping(true);
+            EmergancyAlarmActivity.r.start();
+        }
+        else if (remoteMessage.getNotification().getTitle().equalsIgnoreCase("Broadcast Message!"))
+            intent = new Intent(this, BroadcastNotifiActivity.class);
+
+        if(remoteMessage.getNotification().getTitle().equalsIgnoreCase("Hi your maid has been CheckedIn!")||
+                remoteMessage.getNotification().getTitle().equalsIgnoreCase("Hi your maid has been CheckedOut!"))
+            intent = new Intent(this, SplashActivity.class);
+
+        intent.putExtra("NOTIFICATION_VALUE", remoteMessage);
+        sendNotification(remoteMessage.getNotification().getBody(), remoteMessage.getNotification().getTitle());*/
+
+
+
+
+
+
 
         intent = new Intent(this, VisitorNotificationActivity.class);
         if (remoteMessage.getNotification().getTitle().equalsIgnoreCase("Hi Guest is waiting!"))
@@ -54,7 +89,56 @@ public class SocietyMessagingService extends FirebaseMessagingService {
             intent = new Intent(this, SplashActivity.class);
 
         intent.putExtra("NOTIFICATION_VALUE", remoteMessage);
-        sendNotification(remoteMessage.getNotification().getBody(), remoteMessage.getNotification().getTitle());
+       // Intent notificationIntent = new Intent(this, SplashActivity.class);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence nameChannel = this.getString(R.string.app_name);
+            String descChannel = this.getString(R.string.app_name);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(this.getString(R.string.app_name), nameChannel, importance);
+            channel.setDescription(descChannel);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity((this), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Create Notification
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, this.getString(R.string.app_name))
+                .setChannelId(this.getString(R.string.app_name))
+                .setContentTitle(TextUtils.isEmpty(remoteMessage.getNotification().getTitle()) ? getString(R.string.app_name) :remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setTicker(this.getString(R.string.app_name))
+                .setSmallIcon(R.drawable.ic)
+                .setSound(notificationSound)
+                .setLights(Color.RED, 3000, 3000)
+                .setVibrate(new long[]{500, 500})
+                .setWhen(System.currentTimeMillis())
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+     /*   if (result != null) {
+            notification.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(result));
+        }*/
+
+        assert notificationManager != null;
+        notificationManager.notify(100, notification.build());
+
+
+
+
     }
     // [END receive_message]
 
@@ -65,6 +149,7 @@ public class SocietyMessagingService extends FirebaseMessagingService {
      */
     private void sendNotification(String messageBody, String title) {
         id++;
+        long[] pattern = {500,500,500,500,500};
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, id /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
@@ -75,6 +160,7 @@ public class SocietyMessagingService extends FirebaseMessagingService {
                 .setContentTitle(title)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
+                .setVibrate(pattern)
                 .setOngoing(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
@@ -87,6 +173,8 @@ public class SocietyMessagingService extends FirebaseMessagingService {
     @Override
     public void handleIntent(Intent intent) {
         //super.handleIntent(intent);
+        Log.e("**********","******************handle intent**************************");
+
         try {
             if (intent.getExtras() != null) {
                 RemoteMessage.Builder builder = new RemoteMessage.Builder("SocietyMessagingService");
